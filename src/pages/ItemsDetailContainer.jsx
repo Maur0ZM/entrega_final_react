7058fcc2fd1aebc6c8312258c9e184b6f3a5724c
Mauro0Z <mauro.zm02@gmail.com>
2024-09-05 10:59:06 -0400

    estilos a itemsDetailContainer

--- src/pages/ItemsDetailContainer.jsx
+++ src/pages/ItemsDetailContainer.jsx
@@ -4,21 +4,23 @@ import productos from "../data/data";
 function ItemsDetailContainer() {
     const { productoId } = useParams();
 
-    // Convertir productoId a número para que coincida con el id
     const resultadoFind = productos.find((producto) => producto.id === parseInt(productoId));
 
     if (!resultadoFind) {
-        return <p>Producto no encontrado</p>; // Maneja el caso cuando el producto no existe
+        return <p>Producto no encontrado</p>; 
     }
 
     const { image, title, price } = resultadoFind;
 
     return (
-        <div>
-            <img src={image} alt={title} />
-            <h2>{title}</h2>
-            <p>{price}</p>
-        </div>
+        <section className="w-full justify-center flex mt-10 mb-10">
+            <article className="">
+                <img src={image} alt={title} />
+                <h2>{title}</h2>
+                <p>{price}</p>
+             </article>
+        </section>
+        
     );
 }
 
