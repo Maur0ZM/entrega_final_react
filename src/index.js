dfb3f38cd4e777df97e649100cd4ff9c13dcb574
Mauro0Z <mauro.zm02@gmail.com>
2024-09-15 02:38:19 -0300

    a

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_ZrShnV5R4Z55kEppYirGAZAmL8M-kQ4",
-    authDomain: "react-joyas-pam.firebaseapp.com",
-    projectId: "react-joyas-pam",
-    storageBucket: "react-joyas-pam.appspot.com",
-    messagingSenderId: "928172581339",
-    appId: "1:928172581339:web:5be07c0ce67b93b3a547fe"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
